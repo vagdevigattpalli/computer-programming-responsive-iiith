6ead03130f6a36594ff15bca7b086c148e8c886a
vagdevigattpalli <70190084+vagdevigattpalli@users.noreply.github.com>
2020-09-05 16:53:48 +0530

    Update array.js

--- src/lab/exp4/simulation/1-D/array.js
+++ src/lab/exp4/simulation/1-D/array.js
@@ -118,6 +118,7 @@ window.view = {
 		image.style.top = top  + 'px'
 		image.style.opacity = '0.7'
         image.className = 'arrowImage'
+		image.setAttribute('src','arrowSmall.png')
 		document.getElementById('sortingDiv').appendChild(image)
 	},
 	showCode: function() {
